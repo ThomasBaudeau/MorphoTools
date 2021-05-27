85364e37670942bc3e73d2f047994204c9b9ee2e
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-05-27 21:02:50 +0200

    ok

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -8,3 +8,14 @@ $('#titre').mouseout(
     function(){
         document.querySelector('.infobulle').style.display='none';
     })
+
+//Fenêtre d'importation de fichiers
+document.getElementById('import').addEventListener('click',
+    function () {
+        document.querySelector('.import-modal').style.display = 'flex';
+    });
+
+document.getElementById('import-close').addEventListener('click',
+    function () {
+        document.querySelector('.import-modal').style.display = 'none';
+    });
