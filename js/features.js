1b574eb7c53b1e6b2f387022a912c9528d96dacb
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-05-28 14:10:45 +0200

    ok

--- js/features.js
+++ js/features.js
@@ -27,10 +27,12 @@ function() {
 
 
 //Lancement de Visugraph
+/*
 document.getElementById('visugraph').addEventListener('click',
 function() {
   document.querySelector('.visu').style.display = 'flex';
 });
+*/
 //Ajouter un bouton close
 
 
