beb580fb669ba3987ba4b029f637520a6f80604f
patrickJacques <patrickjacques@laposte.net>
2021-05-31 17:43:08 +0200

    lancement de visugraph, perhaps ?

--- js/forProjectPage.js
+++ js/forProjectPage.js
@@ -4,3 +4,20 @@ function replaceTtitle(){
     document.getElementById('titre').innerHTML = name_project;
     document.getElementById('ok').innerHTML = abstract_project;
 }
+
+function checkClassValue(){
+    var elements = document.getElementsByClassName("select-box");
+    var value = elements[0].value;
+    console.log(value);
+}
+
+//Lancement de Visugraph
+function visuLaunch(){
+    var element = getElementsByClassName("select-box");
+    if(element[0].value == 'VISU'){
+        document.getElementById('btn_run').addEventListener('click',
+        function() {
+            document.querySelector('.visu').style.display = 'flex';
+        });
+    }
+}
