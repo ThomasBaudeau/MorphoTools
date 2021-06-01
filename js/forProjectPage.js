e89fb63456d8e21025fcae0e8f9d5767ae5bb943
GregoryBordier <bordier.gregory@outlook.fr>
2021-06-01 10:29:31 +0200

    bouton run

--- js/forProjectPage.js
+++ js/forProjectPage.js
@@ -4,3 +4,23 @@ function replaceTtitle(){
     document.getElementById('titre').innerHTML = name_project;
     document.getElementById('ok').innerHTML = abstract_project;
 }
+
+function check_tool(){
+    let tl = document.getElementsByName("tools");
+    for (let i = 0; i<tl.length; i++){
+        if (tl[i].checked){
+            let tlc = tl[i].attributes[3].value;
+            return tlc;
+        }
+    }   
+    return null;
+}
+
+function run_tool(){
+    let tl = check_tool();
+    if (tl == "VISU"){
+        document.querySelector('.visu').style.display ='flex';
+    }
+}
+
+document.getElementById('btn_run').addEventListener('click',run_tool())
