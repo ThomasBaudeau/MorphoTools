03e7e7cb66ea2705a5a57588e738fe5c8edcfaf6
GregoryBordier <bordier.gregory@outlook.fr>
2021-06-02 15:01:05 +0200

    safe and sound

--- js/shortsave.js
+++ js/shortsave.js
@@ -10,16 +10,15 @@ function shortSave(cy){
         console.log("short save done");
     }
     
-    function loadShortSave(cy,input){
-    //Permet de réaliser un équivalent d'UNDO à la précédente sauvegarde
-    //load the graph kept in the sessionStorage
-        cy.elements().remove();
-        cy.json({ elements: JSON.parse( window.sessionStorage.getItem("elements") ).elements }).layout({ name: 'preset' }).run();
-        cy.add(input);
-        nodes = cy.nodes();
-        for (var j = 0; j < nodes.length; j++){
-            id = nodes[j].data("id");
-            nodes[j].style("background-image", fileURIs.get(id));
-        }
-        console.log("loaded json");
+function loadShortSave(cy){
+//Permet de réaliser un équivalent d'UNDO à la précédente sauvegarde
+//load the graph kept in the sessionStorage
+    cy.elements().remove();
+    cy.json({ elements: JSON.parse( window.sessionStorage.getItem("elements") ).elements }).layout({ name: 'preset' }).run();
+    nodes = cy.nodes();
+    for (var j = 0; j < nodes.length; j++){
+        id = nodes[j].data("id");
+        nodes[j].style("background-image", fileURIs.get(id));
     }
+    console.log("loaded json");
+}
