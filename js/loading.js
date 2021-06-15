7c1a682bd7b42e95f8f164e285197f7cb6f30b0a
Valentin-Gonay <valentin.gonay@laposte.net>
2021-06-15 14:11:10 +0200

    fixe : nom de projet long ne casse plus l'affichage dans index

---
+++ js/loading.js
@@ -0,0 +1,29 @@
+/* 
+Thomas Baudeau / Gregory Bordier / Valentin Gomay / GOMES Enzo / JACQUES Patrick / SAUVESTRE Clément
+fonctions de chargement et blockage des bouttons
+*/
+
+function loadStart(function_name){
+    //toggle download bar
+        document.querySelector('#download_symbol').style.display = 'block';
+        document.querySelector('#loading_div').style.display = 'block';
+        document.getElementById('loading_message').innerHTML = function_name;
+        blocking_buttons();
+    }
+    
+    function loadEnd(){
+        document.querySelector('#download_symbol').style.display = 'none';
+        document.querySelector('#loading_div').style.display = 'none';
+        document.getElementById('loading_message').innerHTML = "";
+        activating_buttons();
+    }
+    
+    function blocking_buttons(){
+        $(':button').prop('disabled', true);
+        $('input').prop('disabled', true);
+    }
+    
+    function activating_buttons(){
+        $(':button').prop('disabled', false);
+        $('input').prop('disabled', false);
+    }
