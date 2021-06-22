d8cf8be147084925f4454357347a2f40ec68e254
Valentin Gonay <valentin.gonay@laposte.net>
2021-06-22 14:57:43 +0200

    assemble deleted

--- js/assembly.js
+++ js/assembly.js
@@ -5,10 +5,7 @@ Thomas Baudeau / Gregory Bordier / Valentin Gomay / GOMES Enzo / JACQUES Patrick
 var li_nodes=[];
 
 function choose_grp(cy) {
-    document.querySelector('#assemble_div').style.display = 'block';
-    setTimeout(function(){
-        document.querySelector('#assemble_div').style.display = 'block';
-    },1000)
+
 
     cy.nodes().on('click', function(evt) {
             li_nodes.push(evt.target);
