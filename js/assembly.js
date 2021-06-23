9f6a81e515f503cabdb4328687b0596e79930917
Thomas Baudeau <thomas.baudeau.33@gmail.com>
2021-06-23 11:08:09 +0200

    ok

--- js/assembly.js
+++ js/assembly.js
@@ -30,6 +30,9 @@ class Node{
     static AddEdge(id,prob){
         this.link.push({'id':id,'prob':prob});
     }
+    static Getid(){
+        return this.id;
+    }
 }
 
 class Assembly{
@@ -38,11 +41,42 @@ class Assembly{
         this.name=name
     }
     static makeJson(){
+        let json = 
+    {"elements": {
+      "nodes" : [],
+      "edges" : []
+    },
+    "style": [
+      {"selector":"node","style":{"height":"10px","width":"10px","shape":"rectangle","background-fit":"cover","border-color":"rgb(0,0,0)","border-width":"0px","border-opacity":"0.5"}},
+      {"selector":"edge","style":{"width":"0.1px","target-arrow-shape":"triangle","arrow-scale":"0.1","line-color":"rgb(0,0,0)","target-arrow-color":"rgb(0,0,0)"}}
+    ],
+    "data":{},
+    "zoomingEnabled":true,
+    "userZoomingEnabled":true,
+    "zoom":6.009433962264151,
+    "minZoom":1e-50,
+    "maxZoom":1e+50,
+    "panningEnabled":true,
+    "userPanningEnabled":true,
+    "pan":{"x":-2357.7169811320755,"y":255},
+    "boxSelectionEnabled":false,
+    "renderer":{"name":"canvas"}
+  };
         for (node in this.nodes){
-            //construire un JSON
+            let data = {
+                "data":{
+                  "id": node.Getid(),
+                  "label": node-1
+                },
+                "position":{"x":val_x,"y":val_y},
+                "group":"nodes",
+                "removed":false,"selected":false,"selectable":true,"locked":false,"grabbable":true,"pannable":false,"classes":""
+              };
+              json.elements.nodes.push(data);
+            }
         }
-    }
 }
+
 
 ///////////////////////////////////////
 
