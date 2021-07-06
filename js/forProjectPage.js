286c3a34130dcdd0b165b49fd4d84bfdf1902371
Valentin-Gonay <valentin.gonay@laposte.net>
2021-07-06 15:16:31 +0200

    ajout et deletion d'input image fonctionnel

--- js/forProjectPage.js
+++ js/forProjectPage.js
@@ -57,23 +57,50 @@ document.getElementById('btn_run').addEventListener('click',function(){
     }
 })
 
+
 document.getElementById('back_visu').addEventListener('click',function(){
     document.querySelector('#menu_general').style.display = 'block';
     document.querySelector('#menu_visu').style.display = 'none';
     document.querySelector('#display_window').style.display = 'none';
 })
 
+
 function choice_input_type(){
-    document.querySelector('.type_choice-modal').style.display = 'flex'
+    document.querySelector('.type_choice-modal').style.display = 'flex';
 }
+
 
 function create_input_img(type){
     //type: int (0 to 3)
-    let find_type = input_type_list.indexOf(type)
+    let find_type = input_type_list.indexOf(type);
+    let parent_div = document.getElementById('upload');
 
     if (type === 0 && find_type === -1){
         //recto (color)
         console.log("input type : "+type);
+
+        let div = document.createElement('div');
+        div.setAttribute('id','import_div');
+        
+        let label = document.createElement('h3');
+        label.innerHTML = "Import recto (color)";
+        
+        let input = document.createElement('input');
+        input.setAttribute('type','file');
+        input.setAttribute('multiple','true')
+        input.setAttribute('accept','image/*');
+        input.setAttribute('id','ii');
+
+        let img = document.createElement('img');
+        img.setAttribute('src','images/minus_input.png');
+        img.setAttribute('id','minus_input_type');
+        img.setAttribute('onclick','delete_input_img(0,this)')
+
+        div.appendChild(label);
+        div.appendChild(input);
+        div.appendChild(img);
+        parent_div.appendChild(div);
+
         input_type_list.push(type);
         document.querySelector('.type_choice-modal').style.display = 'none';
     }
@@ -81,6 +108,29 @@ function create_input_img(type){
     else if (type === 1 && find_type === -1){
         //verso (color)
         console.log("input type : "+type);
+
+        let div = document.createElement('div');
+        div.setAttribute('id','import_div');
+        
+        let label = document.createElement('h3');
+        label.innerHTML = "Import verso (color)";
+        
+        let input = document.createElement('input');
+        input.setAttribute('type','file');
+        input.setAttribute('multiple','true')
+        input.setAttribute('accept','image/*');
+        input.setAttribute('id','iv');
+
+        let img = document.createElement('img');
+        img.setAttribute('src','images/minus_input.png');
+        img.setAttribute('id','minus_input_type');
+        img.setAttribute('onclick','delete_input_img(1,this)')
+
+        div.appendChild(label);
+        div.appendChild(input);
+        div.appendChild(img);
+        parent_div.appendChild(div);
+
         input_type_list.push(type);
         document.querySelector('.type_choice-modal').style.display = 'none';
 
@@ -89,6 +139,29 @@ function create_input_img(type){
     else if (type === 2 && find_type === -1){
         //recto (infrared)
         console.log("input type : "+type);
+
+        let div = document.createElement('div');
+        div.setAttribute('id','import_div');
+        
+        let label = document.createElement('h3');
+        label.innerHTML = "Import recto (Infrared)";
+        
+        let input = document.createElement('input');
+        input.setAttribute('type','file');
+        input.setAttribute('multiple','true')
+        input.setAttribute('accept','image/*');
+        input.setAttribute('id','ir');
+
+        let img = document.createElement('img');
+        img.setAttribute('src','images/minus_input.png');
+        img.setAttribute('id','minus_input_type');
+        img.setAttribute('onclick','delete_input_img(2,this)')
+
+        div.appendChild(label);
+        div.appendChild(input);
+        div.appendChild(img);
+        parent_div.appendChild(div);
+
         input_type_list.push(type);
         document.querySelector('.type_choice-modal').style.display = 'none';
 
@@ -97,6 +170,29 @@ function create_input_img(type){
     else if (type === 3 && find_type === -1){
         //verso (infrared)
         console.log("input type : "+type);
+
+        let div = document.createElement('div');
+        div.setAttribute('id','import_div');
+        
+        let label = document.createElement('h3');
+        label.innerHTML = "Import recto (Infrared)";
+        
+        let input = document.createElement('input');
+        input.setAttribute('type','file');
+        input.setAttribute('multiple','true')
+        input.setAttribute('accept','image/*');
+        input.setAttribute('id','is');
+
+        let img = document.createElement('img');
+        img.setAttribute('src','images/minus_input.png');
+        img.setAttribute('id','minus_input_type');
+        img.setAttribute('onclick','delete_input_img(3,this)');
+
+        div.appendChild(label);
+        div.appendChild(input);
+        div.appendChild(img);
+        parent_div.appendChild(div);
+
         input_type_list.push(type);
         document.querySelector('.type_choice-modal').style.display = 'none';
 
@@ -107,4 +203,11 @@ function create_input_img(type){
     }
     
     
+}
+
+function delete_input_img(type,self){
+    let find_type = input_type_list.indexOf(type);
+    input_type_list.splice(find_type,1);
+    self.parentNode.parentNode.removeChild(self.parentNode)
+
 }
